a10ba2266cb0a630a550e4c4e1712d0a2eb9adfe
SSsiriusz <168542258+SSsiriusz@users.noreply.github.com>
2024-05-15 10:55:52 -0300

    Add files via upload

---
+++ atividade 10 - 05.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	float valor1;
+	float valor2;
+		 
+	printf ("Escreva o valor1: ");
+	scanf ("%f", &valor1);
+	
+	printf ("Escreva o valor2: ");
+	scanf ("%f", &valor2);
+	
+    float total = valor1 / valor2;
+    
+    printf ("A divisao entre valores sao: %f",total);
+	 
+	return 0;
+	
+	
+}
+
