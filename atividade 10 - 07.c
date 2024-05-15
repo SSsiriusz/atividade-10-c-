c484e4950e5cb5dd82b8920a94caacd4e62fc30f
SSsiriusz <168542258+SSsiriusz@users.noreply.github.com>
2024-05-15 11:12:49 -0300

    Add files via upload

---
+++ atividade 10 - 07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	float valor1;
+		 
+	printf ("Escreva o peso do seu prato em kilos: \n ");
+	scanf ("%f \n \n ", &valor1);
+	
+    
+    float total = (valor1 * 45);
+    
+    
+	printf ("O valor total do seu prato é: %f\n \n ",total);
+	 
+	return 0;
+	
+	
+}
+
