159cbab1828962175b1ea3b8384b924089ccacd4
SSsiriusz <168542258+SSsiriusz@users.noreply.github.com>
2024-05-15 10:23:47 -0300

    Add files via upload

---
+++ atividade 01 c.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	float nota1;
+	float nota2;
+	float nota3;
+	float nota4;
+		 
+	printf ("Escreva a primeira nota: ");
+	scanf ("%f", &nota1);
+	printf ("Escreva a segunda nota: ");
+	scanf ("%f", &nota2);
+	printf ("Escreva a terceira nota: ");
+	scanf ("%f", &nota3);
+	printf ("Escreva a quarta nota: ");
+	scanf ("%f", &nota4);
+	
+	float total = nota1+nota2+nota3+nota4;
+	
+	printf ("Nota total: %f \n",total);
+	
+	 
+	return 0;
+	
+	
+}
+
